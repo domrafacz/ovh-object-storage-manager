469cb2329475572d7cc63388ed78bc434a913fc0
Dominik Rafacz <dominik.rafacz@protonmail.com>
2022-07-17 23:42:09 +0200

    add config permission check

--- public/index.php
+++ public/index.php
@@ -1,7 +1,11 @@
 <?php
 
 if (!file_exists(__DIR__ . '/../src/config.php')) {
-    copy(__DIR__ . '/../src/config.example.php', __DIR__ . '/../src/config.php');
+    if (is_writable(__DIR__ . '/../src/')) {
+        copy(__DIR__ . '/../src/config.example.php', __DIR__ . '/../src/config.php');
+    } else {
+        die('Permission denied when creating config file: ' . realpath(__DIR__ . '/../src') . '/config.php');
+    }
 }
 
 session_start();
